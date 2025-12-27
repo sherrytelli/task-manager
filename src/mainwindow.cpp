6cbe6603f81d73fb842717aa77f86cead0308570
sherrytelli <sheheryarsalman186@hotmail.com>
2025-12-27 19:12:16 +0500

    removed label, widget and layout initialization from class MainWindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,20 +1,12 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), CentralWidget(new QWidget(this)), Layout(new QVBoxLayout(this)), HelloLabel(new QLabel(this)) {
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
     setupUI();
 }
 
 MainWindow::~MainWindow() {}
 
 void MainWindow::setupUI() {
-    HelloLabel->setAlignment(Qt::AlignCenter);
-    HelloLabel->setStyleSheet("QLabel { font-size: 24px; font-weight: bold; color: #333; }");
-    
-    Layout->addWidget(HelloLabel);
-    Layout->setAlignment(Qt::AlignCenter);
-    
-    setCentralWidget(CentralWidget);
-    
     setWindowTitle("Task Manager");
-    resize(400, 300);
+    resize(600, 800);
 }
