894d5c15ff178bbaeeed19fdeb94226ddf52cddc
sherrytelli <sheheryarsalman186@hotmail.com>
2025-12-28 00:11:11 +0500

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include "mainwindow.h"
 
 int main (int argc, char *argv[]) {
+    //creates the main QApplication
     QApplication app(argc, argv);
 
+    //creates the mainwindow for the app
     MainWindow window;
     window.show();
 
+    //returns the app event loop
     return app.exec();
 }
