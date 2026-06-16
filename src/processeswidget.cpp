74db7254d18b065cc5e87d3142da1f7688e9fd71
sherrytelli <sheheryarsalman186@hotmail.com>
2026-06-16 14:14:54 +0500

    chore: show only total memory in column

--- src/processeswidget.cpp
+++ src/processeswidget.cpp
@@ -209,7 +209,7 @@ void ProcessWidget::updateProcessesList() {
         cpuItem->setTextAlignment(Qt::AlignCenter);
         tableWidget->setItem(row, COL_CPU_PERCENT, cpuItem);
 
-        QTableWidgetItem *memItem = new QTableWidgetItem(QString("%1 / %2 (%3)").arg(formatMemorySize(info.rssBytes), formatMemorySize(info.unsharedRssBytes), formatMemorySize(info.rssBytes + info.unsharedRssBytes)));
+        QTableWidgetItem *memItem = new QTableWidgetItem(formatMemorySize(info.rssBytes + info.unsharedRssBytes));
         memItem->setData(Qt::DisplayRole, info.rssBytes + info.unsharedRssBytes);
         memItem->setTextAlignment(Qt::AlignCenter);
         tableWidget->setItem(row, COL_MEMORY, memItem);
