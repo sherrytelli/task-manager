e3ab424592e19cfee0096ebb60fb607a4b417fd3
sherrytelli <sheheryarsalman186@hotmail.com>
2026-06-16 13:22:21 +0500

    added status bar to show process count and memory usage

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,20 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+#include <QDateTime>
+#include <QScreen>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      statusLabel(new QLabel(this)),
+      memoryLabel(new QLabel(this)),
+      refreshLabel(new QLabel(this)) {
     setWindowTitle("Task Manager");
-    resize(600, 700);
+
+    QRect screenGeometry = QGuiApplication::primaryScreen()->availableGeometry();
+    int defaultWidth = qMin(900, screenGeometry.width() - 100);
+    int defaultHeight = qMin(750, screenGeometry.height() - 100);
+    resize(defaultWidth, defaultHeight);
+
     init();
 }
 
@@ -16,15 +28,16 @@ void MainWindow::init() {
     processesScreen = new ProcessWidget(this);
 
     metricsScreen->setAlignment(Qt::AlignCenter);
-    metricsScreen->setStyleSheet("font-size: 24px; background-color: black");
+    metricsScreen->setStyleSheet("font-size: 24px; background-color: #1a1a2e; color: #e0e0e0;");
 
     stackedWidget = new QStackedWidget(this);
     stackedWidget->addWidget(metricsScreen);
     stackedWidget->addWidget(processesScreen);
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
     layout->addWidget(stackedWidget);
-    centralWidget->setLayout(layout);
 
     QToolBar *toolBar = new QToolBar(this);
     toolBar->setMovable(false);
@@ -43,6 +56,16 @@ void MainWindow::init() {
     connect(metricsAction, &QAction::triggered, this, &MainWindow::showMetricsScreen);
     connect(processesAction, &QAction::triggered, this, &MainWindow::showProcessesScreen);
 
+    // Status bar
+    QStatusBar *statusBar = this->statusBar();
+    statusBar->addWidget(statusLabel);
+    statusBar->addPermanentWidget(memoryLabel);
+    statusBar->addPermanentWidget(refreshLabel);
+
+    updateStatusBar(0, 0, 0);
+
+    connect(processesScreen, &ProcessWidget::refreshComplete, this, &MainWindow::updateStatusBar);
+
     showMetricsScreen();
 }
 
@@ -56,4 +79,27 @@ void MainWindow::showProcessesScreen() {
     stackedWidget->setCurrentIndex(1);
     metricsAction->setEnabled(true);
     processesAction->setEnabled(false);
+}
+
+void MainWindow::updateStatusBar(int processCount, quint64 totalMem, quint64 usedMem) {
+    statusLabel->setText(QString("Processes: %1").arg(processCount));
+
+    auto formatMem = [](quint64 bytes) -> QString {
+        double mb = static_cast<double>(bytes) / (1024.0 * 1024.0);
+        return QString("Memory: %1 MB").arg(mb, 0, 'f', 1);
+    };
+
+    if (totalMem > 0) {
+        double totalMb = static_cast<double>(totalMem) / (1024.0 * 1024.0);
+        double usedMb = static_cast<double>(usedMem) / (1024.0 * 1024.0);
+        memoryLabel->setText(QString("Memory: %1 / %2 MB (%3%)")
+            .arg(usedMb, 0, 'f', 1)
+            .arg(totalMb, 0, 'f', 1)
+            .arg(static_cast<int>((static_cast<double>(usedMem) / totalMem) * 100.0)));
+    } else {
+        memoryLabel->setText(formatMem(usedMem));
+    }
+
+    refreshLabel->setText(QString("Last refresh: %1")
+        .arg(QDateTime::currentDateTime().toString("HH:mm:ss")));
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,6 +5,7 @@
 #include <QLabel>
 #include <QMainWindow>
 #include <QStackedWidget>
+#include <QStatusBar>
 #include <QToolBar>
 #include <QVBoxLayout>
 #include "processeswidget.h"
@@ -22,9 +23,13 @@ class MainWindow : public QMainWindow {
     QStackedWidget *stackedWidget;
     QAction *metricsAction;
     QAction *processesAction;
+    QLabel *statusLabel;
+    QLabel *memoryLabel;
+    QLabel *refreshLabel;
     void showMetricsScreen();
     void showProcessesScreen();
     void init();
+    void updateStatusBar(int processCount, quint64 totalMem, quint64 usedMem);
 };
 
 #endif // MAINWINDOW_H
